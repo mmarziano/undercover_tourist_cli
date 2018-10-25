865e2c31e8d29a27fed02255632b0cc8980df080
Monica Marziano <mmarziano1@gmail.com>
2018-10-25 09:10:28 +0000

    Added scraper file

---
+++ lib/undercover_tourist_cli/scraper.rb
@@ -0,0 +1,14 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper 
+  
+  @base_path = "https://www.undercovertourist.com/"
+
+
+  def scrape_attractions(city)
+  
+  
+  
+end 
