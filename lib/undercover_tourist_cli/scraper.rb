5f30b5dc2f50fbdc2f13899fc43b8bc28d9ba138
Monica Marziano <mmarziano1@gmail.com>
2018-11-16 00:15:53 +0000

    Done.

--- lib/undercover_tourist_cli/scraper.rb
+++ lib/undercover_tourist_cli/scraper.rb
@@ -29,7 +29,8 @@ class Scraper
   def self.scrape_city_summary
     @page = Nokogiri::HTML(open(@base_path + "/#{@city}"))
     @city_attractions[:city_summary] = @page.css(".cityblurb").children.css("p").text
-    City.new(@city, @city_attractions[:city_summary])
+    x = City.new(@city, @city_attractions[:city_summary])
+    puts City.city_summary
   end 
   
   def self.scrape_city_attractions
