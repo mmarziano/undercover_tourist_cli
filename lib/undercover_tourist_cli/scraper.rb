628b74c5ba955dbf453ca65a78dae31267fcc33e
Monica Marziano <mmarziano1@gmail.com>
2018-11-20 11:19:44 +0000

    Done.

--- lib/undercover_tourist_cli/scraper.rb
+++ lib/undercover_tourist_cli/scraper.rb
@@ -72,14 +72,73 @@ class Scraper
       end
     end
     Attractions.name=(@selected_attraction)
-    Scraper.scrape_attraction_rating
-  end
+    Scraper.scrape_details
   
-  def self.parse_selected_attration_page
+  def self.parse_selected_attraction_page
       @page = Nokogiri::HTML(open(@selected_attraction_url))
       return @page
   end 
-  
+
+  def self.scrape_details
+    Scraper.parse_selected_attraction_page
+    node = @page.css('.reviewpads')
+    if node.empty?
+      @city_attractions[:rating] = "N/A"
+    else
+      node1 = @page.css('.reviewpads').attribute('class').value.split[1].split('star')
+      @city_attractions[:rating] = node1[0].capitalize + " Stars"
+    end
+      Attractions.rating=(@city_attractions[:rating])
+     node2 = @page.css('.about-attraction').children.css('p').text
+        if node2.empty?
+          @city_attractions[:description] = "N/A"
+        else
+         @city_attractions[:description] = node2
+        end
+        Attractions.description=(@city_attractions[:description])
+      node3 = @page.css('.daydetail')
+         if node3.nil?
+           @city_attractions[:current_crowd_rating] = "N/A"
+         else
+           @city_attractions[:current_crowd_rating] = node3.first.text
+         end
+         Attractions.current_crowd_rating=(@city_attractions[:current_crowd_rating])  
+      @priority_attractions = []
+        node4 = @page.css('.fff-attractions').children.css('li').children.css('a')
+             node4.each do |node|
+               @priority_attractions << node.text
+             end
+         if @priority_attractions.empty?
+             @city_attractions[:priority_attractions] = "N/A"
+           else 
+             @city_attractions[:priority_attractions] = @priority_attractions
+           end
+          Attractions.priority_attractions=(@city_attractions[:priority_attractions])
+        node5 = @page.css('.calattraction').attribute('data-filter-ids').value
+          if node5.include?('None')
+            @city_attractions[:hours] = "N/A"
+          elsif 
+            node6 = @page.css('calattraction .filterableitem').nil?
+              @city_attractions[:hours] = "N/A"
+          elsif 
+            node6 = @page.css('.calattraction .filterableitem .caltime')[0].nil?
+              @city_attractions[:hours] = "N/A"
+          else 
+            node6 = @page.css('.calattraction .filterableitem .caltime')[0].text.strip
+            if node6 == nil
+              @city_attractions[:hours] = "N/A"
+            elsif node6.include?('EMH')
+              node7 = @page.css('.calattraction .filterableitem .caltime')[1].text.strip
+                 hours = "#{node6} " + "/ #{node7}"
+                 @city_attractions[:hours] = hours
+            else 
+                hours = "#{node6}" 
+                 @city_attractions[:hours] = hours
+            end 
+          end  
+          Attractions.hours=(@city_attractions[:hours])
+          UndercoverTouristCli::Cli.results
+  end 
   
   def self.scrape_attraction_rating
     @page = Nokogiri::HTML(open(@selected_attraction_url))
