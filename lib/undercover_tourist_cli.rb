c9457125843e25e54da7dd8088bba3518c63fe0a
Monica Marziano <mmarziano1@gmail.com>
2018-12-14 10:35:44 +0000

    Done.

--- lib/undercover_tourist_cli.rb
+++ lib/undercover_tourist_cli.rb
@@ -52,22 +52,9 @@ class UndercoverTouristCli::Cli
           @selected_attraction = val
         end
       end 
+    puts "Gathering details for #{@selected_attraction.name}..."
     Scraper.attraction_details(@selected_attraction)
-    #node = @attraction_page.css('.tile')
-      #node.each do |node|
-       #url = node.children.css('a').attribute('href')
-        #if url != nil
-           #@attraction_url = @base_path + "#{url}"
-           #@attraction_urls << @attraction_url
-        #end
-      #end 
-      #@attraction_urls.select.with_index do |val, index|
-        #if input == index.to_i + 1
-          #@selected_attraction_url = val
-        #end
-      #end
-    #Scraper.scrape_details
-    
+    results(@selected_attraction)
   end
   
   
@@ -78,8 +65,8 @@ class UndercoverTouristCli::Cli
       puts "Below is a list of attractions:"
       puts "-------------------------------"
             i = 1
-            Scraper.attractions.each do |attraction|
-              puts "#{i}.".colorize(:red) + " #{attraction}".colorize(:blue)
+            Attractions.all.each do |attraction|
+              puts "#{i}.".colorize(:red) + " #{attraction.name}".colorize(:blue)
               i += 1
             end 
           puts "Please select a number from the list above."
@@ -91,24 +78,25 @@ class UndercoverTouristCli::Cli
     end
   end 
   
-    def results
+    def results(attraction)
       puts "---------------------------------------------------".colorize(:red)
-      puts "***#{Attractions.name}***"
+      puts "***#{attraction.name}***"
       puts "---------------------------------------------------".colorize(:red)
       puts ""
-      puts "Attraction Description: ".colorize(:red) + Attractions.description
-      puts "Attraction Rating: ".colorize(:red) + Attractions.rating 
-      puts "Today's Attraction Crowd Size (Scale 1-10): ".colorize(:red) + Attractions.current_crowd_rating
-      puts "Today's Attraction Hours: ".colorize(:red) + Attractions.hours 
+      puts "Attraction Description: ".colorize(:red) + attraction.description
+      puts "Attraction Rating: ".colorize(:red) + attraction.rating 
+      puts "Today's Attraction Crowd Size (Scale 1-10): ".colorize(:red) + attraction.current_crowd_rating
+      puts "Today's Attraction Hours: ".colorize(:red) + attraction.hours 
       puts "Be sure to check out: ".colorize(:red) 
-        if Attractions.priority_attractions == "N/A"
+        if attraction.priority_attractions == "N/A"
           puts "N/A"
         else 
-          Attractions.priority_attractions.each do |attraction|
+          attraction.priority_attractions.each do |attraction|
             puts attraction
           end 
         end
       puts "Would you like to check out another attraction? (Y/N)".colorize(:cyan)
-      pick_attraction_repeat
+      #pick_attraction_repeat
+      exit
   end
 end
--- lib/undercover_tourist_cli/scraper.rb
+++ lib/undercover_tourist_cli/scraper.rb
@@ -61,7 +61,7 @@ class Scraper
            node4.each do |node|
              attraction.priority_attractions << node.text
            end
-         if attractions.priority_attractions.empty?
+         if attraction.priority_attractions.empty?
            attraction.priority_attractions=("N/A")
          end
     node5 = attraction_info.css('.calattraction').attribute('data-filter-ids').value
@@ -86,82 +86,5 @@ class Scraper
                    attraction.hours=(hours)
               end 
           end  
-
-          #x = Attractions.new(@selected_attraction, @city_attractions[:description], @city_attractions[#:rating], @city_attractions[:current_crowd_rating], @city_attractions[:priority_attractions]#, @city_attractions[:hours])
-    binding.pry
-    
-    
-  end
-  
-  def self.parse_selected_attraction_page(selected_attraction)
-    puts "******* Acquiring Attraction Details *********"
-      @page = Nokogiri::HTML(open(attraction.url))
-      return @page
-  end 
-
-  def self.scrape_details(attraction_url)
-    Scraper.parse_selected_attraction_page(attraction_url)
-    node = @page.css('.reviewpads')
-      if node.empty?
-        @city_attractions[:rating] = "N/A"
-      else
-        node1 = @page.css('.reviewpads').attribute('class').value.split[1].split('star')
-        @city_attractions[:rating] = node1[0].capitalize + " Stars"
       end
-      Attractions.rating=(@city_attractions[:rating])
-     node2 = @page.css('.about-attraction').children.css('p').text
-        if node2.empty?
-          @city_attractions[:description] = "N/A"
-        else
-         @city_attractions[:description] = node2
-        end
-        Attractions.description=(@city_attractions[:description])
-      node3 = @page.css('.daydetail')
-         if node3.nil?
-           @city_attractions[:current_crowd_rating] = "N/A"
-         else
-           @city_attractions[:current_crowd_rating] = node3.first.text
-         end
-         Attractions.current_crowd_rating=(@city_attractions[:current_crowd_rating])  
-      @priority_attractions = []
-        node4 = @page.css('.fff-attractions').children.css('li').children.css('a')
-             node4.each do |node|
-               @priority_attractions << node.text
-             end
-            if @priority_attractions.empty?
-               @city_attractions[:priority_attractions] = "N/A"
-             else 
-             @city_attractions[:priority_attractions] = @priority_attractions
-            end
-          Attractions.priority_attractions=(@city_attractions[:priority_attractions])
-        node5 = @page.css('.calattraction').attribute('data-filter-ids').value
-          if node5.include?('None')
-            @city_attractions[:hours] = "N/A"
-          elsif 
-            node6 = @page.css('calattraction .filterableitem').nil?
-              @city_attractions[:hours] = "N/A"
-          elsif 
-            node6 = @page.css('.calattraction .filterableitem .caltime')[0].nil?
-              @city_attractions[:hours] = "N/A"
-          else 
-            node6 = @page.css('.calattraction .filterableitem .caltime')[0].text.strip
-            if node6 == nil
-              @city_attractions[:hours] = "N/A"
-            elsif node6.include?('EMH')
-              node7 = @page.css('.calattraction .filterableitem .caltime')[1].text.strip
-                 hours = "#{node6} " + "/ #{node7}"
-                 @city_attractions[:hours] = hours
-              else 
-                hours = "#{node6}" 
-                 @city_attractions[:hours] = hours
-            end 
-          end  
-          Attractions.hours=(@city_attractions[:hours])
-          #Attractions.clear
-          x = Attractions.new(@selected_attraction, @city_attractions[:description], @city_attractions[:rating], @city_attractions[:current_crowd_rating], @city_attractions[:priority_attractions], @city_attractions[:hours])
-       
-         
-    end 
-    
-    
-end 
+  end 
